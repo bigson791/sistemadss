6674d430ee5a62fd7728b78579cd8d32184c0ebc
Elfego Home <bigson791@gmail.com>
2025-07-25 01:16:37 -0600

    se agrego el endpoint de actualizar producto

--- backend/routes/Productos.js
+++ backend/routes/Productos.js
@@ -13,4 +13,31 @@ router.post('/', async (req, res) => {
   res.json({ ok: true });
 });
 
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const productoActualizado = await Producto.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true });
+    if (!productoActualizado) {
+      return res.status(404).json({ error: 'Producto no encontrado' });
+    }
+
+    res.json({ ok: true, producto: productoActualizado });
+
+  } catch (error) {
+    console.error(err);
+    res.status(500).json({ error: 'Error al actualizar el producto' });
+  }
+  // console.log(id);
+  // const productoActualizado = await Producto.find({ _id: id });
+  // console.log(productoActualizado);
+  // if (productoActualizado.length === 0) {
+  //   return res.status(404).json({ error: 'Producto no encontrado' });
+  // }
+  // await Producto.updateOne({_id: id }, req.body);
+  // res.json({ ok: true });
+});
+
 module.exports = router;
